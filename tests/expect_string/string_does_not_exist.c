65d84dbbd2e8233e4d3305835c812d5caffb6f10
swandrn <159421702+swandrn@users.noreply.github.com>
2026-09-24 09:58:32 +0200

    test: expect_string string does not exist

---
+++ tests/expect_string/string_does_not_exist.c
@@ -0,0 +1,25 @@
+#include "tat.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+  tat_config config = {
+      .headless = false,
+      .viewer_terminal_path = "ghostty",
+      .viewer_terminal_name = "ghostty",
+      .cols = 100,
+      .rows = 40,
+  };
+
+  tat_session *session = tat_session_create(&config);
+  assert(session != NULL);
+
+  assert(tat_start_program(session, "/usr/bin/htop", "htop") >= 0);
+
+  assert(!tat_expect_string(session, "A String That Does Not Exist", 1000));
+
+  tat_session_destroy(session);
+  return 0;
+}
